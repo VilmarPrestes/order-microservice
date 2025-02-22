d905e44677a2ed8d9a728fe13d4ece7e108cffaa
Vilmar Prestes <prestesvilmar20@gmail.com>
2025-02-22 00:22:02 -0300

    feat: add new entities and changed BR to EN

--- src/main/java/com/VilmarPrestes/order_microservice/model/User.java
+++ src/main/java/com/VilmarPrestes/order_microservice/model/User.java
@@ -3,9 +3,18 @@ package com.VilmarPrestes.order_microservice.model;
 
 import jakarta.persistence.*; //ja add tudo da tabela - importante
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity  // Indica que esta classe é uma entidade do banco
 @Table(name = "users") // Nome da tabela no banco
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
     @Id  // Indica que este campo é a chave primária
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
@@ -13,27 +22,15 @@ public class User {
 
     @Column(nullable = false) // Define que este campo não pode ser nulo
     @NotBlank
-    private String nome;
+    private String name;
 
     @Column(nullable = false, unique = true) // Define que o email é único
     @NotBlank
     private String email;
 
-    // Construtor padrão
-    public User() {}
+    private String password;
 
-    // Construtor com parâmetros
-    public User(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
+    private Integer birthday;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
