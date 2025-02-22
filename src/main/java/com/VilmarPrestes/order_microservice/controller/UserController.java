5dd0537f1219a7d0249cc48f9d46f68e0d9d54fe
Vilmar Prestes <prestesvilmar20@gmail.com>
2025-02-22 00:15:54 -0300

    feat: add new entities and changed BR to EN

--- src/main/java/com/VilmarPrestes/order_microservice/controller/UserController.java
+++ src/main/java/com/VilmarPrestes/order_microservice/controller/UserController.java
@@ -21,15 +21,15 @@ public class UserController {
     private UserRepository userRepository;
 
     @Autowired
-    private UserProducer userProducer; // Injeção do RabbitMQ Producer
+    private UserProducer userProducer; // RabbitMQ Producer Injection
 
     @GetMapping
-    public List<User> listarUsers() {
+    public List<User> listUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> buscarUser(@PathVariable("id") Long id) {
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
@@ -39,8 +39,8 @@ public class UserController {
         try {
             User savedUser = userRepository.save(user);
 
-            // Envia mensagem ao RabbitMQ
-            userProducer.sendMessage("Novo usuário cadastrado: " + savedUser.getNome());
+            // Send message to RabbitMQ
+            userProducer.sendMessage("New user registered: " + savedUser.getName());
 
             return ResponseEntity.status(201).body(savedUser);
         } catch (Exception e) {
@@ -49,19 +49,20 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> atualizarUser(@PathVariable("id") Long id, @RequestBody User dados) {
-        Optional<User> userExistente = userRepository.findById(id);
-        if (userExistente.isPresent()) {
-            User user = userExistente.get();
-            user.setNome(dados.getNome());
-            user.setEmail(dados.getEmail());
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User data) {
+        Optional<User> userExists = userRepository.findById(id);
+        if (userExists.isPresent()) {
+            User user = userExists.get();
+            user.setName(data.getName());
+            user.setEmail(data.getEmail());
+            user.setBirthday(data.getBirthday());
             return ResponseEntity.ok(userRepository.save(user));
         }
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluirUser(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
         if (userRepository.existsById(id)) {
             userRepository.deleteById(id);
             return ResponseEntity.noContent().build();
@@ -70,7 +71,7 @@ public class UserController {
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> excluirTodosUsuarios() {
+    public ResponseEntity<Void> deleteAllUsers() {
         userRepository.deleteAll();
         return ResponseEntity.noContent().build();
     }
